d305b32e999f64724099a79d5b4a06c7b88e735a
Thomas Pecout <pecoutt@hotmail.fr>
2018-11-21 15:39:33 +0100

    installation de webpack

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,13 +10,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/category")
- */
+
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/", name="category_index", methods="GET")
+     * @Route("/category", name="category_index", methods="GET")
      */
     public function index(CategoryRepository $categoryRepository): Response
     {
@@ -24,7 +22,7 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="category_new", methods="GET|POST")
+     * @Route("admin/category/new", name="category_new", methods="GET|POST")
      */
     public function new(Request $request): Response
     {
@@ -47,7 +45,7 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="category_show", methods="GET")
+     * @Route("category/{id}", name="category_show", methods="GET")
      */
     public function show(Category $category): Response
     {
@@ -55,7 +53,7 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="category_edit", methods="GET|POST")
+     * @Route("admin/category/{id}/edit", name="category_edit", methods="GET|POST")
      */
     public function edit(Request $request, Category $category): Response
     {
@@ -75,7 +73,7 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="category_delete", methods="DELETE")
+     * @Route("admin/category/{id}", name="category_delete", methods="DELETE")
      */
     public function delete(Request $request, Category $category): Response
     {
