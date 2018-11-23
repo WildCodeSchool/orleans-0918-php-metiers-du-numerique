faac70fe0928430f3cba71de641459dacce16cf2
Thomas Pecout <pecoutt@hotmail.fr>
2018-11-23 13:41:13 +0100

    correction done

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class CategoryController extends AbstractController
 {
     /**
