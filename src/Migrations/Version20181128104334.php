869e81fb178e39ccb168b4e8d641e62c6866b398
Thomas Pecout <pecoutt@hotmail.fr>
2018-11-28 11:47:49 +0100

    doctrine migrate

---
+++ src/Migrations/Version20181128104334.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181128104334 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE business (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, logo VARCHAR(255) NOT NULL, mail VARCHAR(255) NOT NULL, link VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE business_job (business_id INT NOT NULL, job_id INT NOT NULL, INDEX IDX_65ABD188A89DB457 (business_id), INDEX IDX_65ABD188BE04EA9 (job_id), PRIMARY KEY(business_id, job_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE business_job ADD CONSTRAINT FK_65ABD188A89DB457 FOREIGN KEY (business_id) REFERENCES business (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE business_job ADD CONSTRAINT FK_65ABD188BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id) ON DELETE CASCADE');
+        $this->addSql('DROP TABLE category');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE business_job DROP FOREIGN KEY FK_65ABD188A89DB457');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE business');
+        $this->addSql('DROP TABLE business_job');
+    }
+}
