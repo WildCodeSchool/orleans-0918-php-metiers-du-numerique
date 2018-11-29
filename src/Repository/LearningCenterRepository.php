bb475081f03408cc99ac524eb9d1cfa31ec42854
julomontiti <julomontiti@gmail.com>
2018-11-29 15:41:39 +0100

    code commenté et info dans alt

--- src/Repository/LearningCenterRepository.php
+++ src/Repository/LearningCenterRepository.php
@@ -19,32 +19,4 @@ class LearningCenterRepository extends ServiceEntityRepository
         parent::__construct($registry, LearningCenter::class);
     }
 
-    // /**
-    //  * @return LearningCenter[] Returns an array of LearningCenter objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('l')
-            ->andWhere('l.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('l.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?LearningCenter
-    {
-        return $this->createQueryBuilder('l')
-            ->andWhere('l.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
