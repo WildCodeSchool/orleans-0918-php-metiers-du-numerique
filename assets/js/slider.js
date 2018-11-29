28c83062bd5da183c3269f4ebb6122b900080380
Alicia Pilar <alicia.pilar83@gmail.com>
2018-11-30 00:21:29 +0100

    working but need debug

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,27 +1,29 @@
-$(".single-item").slick({
-    centerPadding: '60px',
-    slidesToShow: 3,
+$('.single-item').slick({
+    centerMode: true,
+    centerPadding: '50px',
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed:3000,
+    slidesToShow: 5,
     responsive: [
         {
             breakpoint: 768,
             settings: {
                 arrows: false,
-                // centerMode: true,
+                centerMode: true,
                 centerPadding: '40px',
-                slidesToShow: 1
+                slidesToShow: 3
             }
         },
         {
             breakpoint: 480,
             settings: {
-                arrows: true,
-                // centerMode: true,
+                arrows: false,
+                centerMode: true,
                 centerPadding: '40px',
                 slidesToShow: 1
             }
         }
     ]
 });
-
-
 			
