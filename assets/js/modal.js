ae4be91253799385f3e5851403fcd15b8802fcc2
Thomas Pecout <pecoutt@hotmail.fr>
2018-11-29 10:31:59 +0100

    add js

---
+++ assets/js/modal.js
@@ -0,0 +1,9 @@
+function openModal() {
+    console.log('string');
+    document.getElementById("myModal").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeModal() {
+    document.getElementById("myModal").style.width = "0%";
+}
