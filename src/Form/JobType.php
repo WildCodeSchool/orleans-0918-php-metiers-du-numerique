91be4622fa87b1abc56baa7f2d83f37ebe88553b
Amelie Aumont <maka.amelie@hotmail.com>
2018-12-06 16:21:38 +0100

    ajout du champ select

--- src/Form/JobType.php
+++ src/Form/JobType.php
@@ -2,16 +2,23 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Job;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class JobType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('associatedCategory', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
             ->add('name')
             ->add('video')
             ->add('picture')
