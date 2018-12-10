1e8f72cd6610c3649bb9811b78b73463b917cc92
Alicia Pilar <alicia.pilar83@gmail.com>
2018-12-10 17:38:12 +0100

    routes update

--- src/Controller/LearningCenterController.php
+++ src/Controller/LearningCenterController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/learning/center")
+ * @Route("/learningCenter")
  */
 class LearningCenterController extends AbstractController
 {
