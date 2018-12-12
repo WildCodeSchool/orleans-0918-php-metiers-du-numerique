e30ce9362bec12143637adba9c87531e33abcf15
Billy-Vivant <billy.vivant@gmail.com>
2018-12-12 15:37:15 +0100

    Fonctionnalite accepter une entreprise fini

--- src/Controller/CompanyAdminController.php
+++ src/Controller/CompanyAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Company;
+use App\Form\AcceptCompanyType;
 use App\Form\CompanyType;
 use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,20 +19,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class CompanyAdminController extends AbstractController
 {
     /**
-     * @Route("/", name="compagny_admin")
+     * @Route("/", name="company_admin", methods="GET|POST" )
      */
     public function index(CompanyRepository $compagnyRepository): Response
     {
+
+
         return $this->render('company_admin/index.html.twig', [
             'companies' => $compagnyRepository->findBy([], ['accepted'=>'ASC'])
         ]);
     }
     /**
-     * @Route("/{id}", name="company_show", methods="GET")
+     * @Route("/{id}", name="company_show", methods="GET|POST")
      */
-    public function show(Company $company): Response
+    public function show(Request $request , Company $company): Response
     {
-        return $this->render('company_admin/show.html.twig', ['company' => $company]);
+        $form = $this->createForm(AcceptCompanyType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $company->setAccepted(!$company->getAccepted());
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('company_admin', ['id' => $company->getId()]);
+        }
+
+        return $this->render('company_admin/show.html.twig', [
+            'company' => $company,
+            'form' => $form->createView(),
+        ]);
     }
     /**
      * @Route("/{id}", name="company_admin_delete", methods="DELETE")
