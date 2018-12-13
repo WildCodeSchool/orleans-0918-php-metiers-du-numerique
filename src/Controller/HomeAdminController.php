83f8dcfc1fbebc2368c5d89f0fa8cfffeced9368
Alicia Pilar <alicia.pilar83@gmail.com>
2018-12-13 21:21:45 +0100

    done and word

--- src/Controller/HomeAdminController.php
+++ src/Controller/HomeAdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
+use App\Repository\CompanyRepository;
+use App\Repository\LearningCenterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +13,17 @@ class HomeAdminController extends AbstractController
     /**
      * @Route("/admin/", name="home_admin")
      */
-    public function index()
+    public function index(CompanyRepository $companyRepository, CommentRepository $commentRepository, LearningCenterRepository $learningCenterRepository)
     {
+        $companies = $companyRepository->findByAccepted(0);
+        $comments = $commentRepository->findByAccepted(0);
+        $learningCenters = $learningCenterRepository->findByAccepted(0);
+
         return $this->render('home_admin/index.html.twig', [
             'controller_name' => 'HomeAdminController',
+            'companies'=>$companies,
+            'comments'=>$comments,
+            'learningCenters'=>$learningCenters
         ]);
     }
 }
