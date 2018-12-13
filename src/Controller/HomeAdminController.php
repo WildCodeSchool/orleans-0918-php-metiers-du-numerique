4c778989f8c14764e84abf3574758abd11cf25bb
Alicia Pilar <alicia.pilar83@gmail.com>
2018-12-13 09:35:17 +0100

    refactor routes

--- src/Controller/HomeAdminController.php
+++ src/Controller/HomeAdminController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeAdminController extends AbstractController
 {
     /**
-     * @Route("/admin/home", name="home_admin")
+     * @Route("/admin/", name="home_admin")
      */
     public function index()
     {
