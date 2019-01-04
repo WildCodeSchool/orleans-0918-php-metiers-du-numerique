5402f79033c375654bda3c2295d83c015759ae7b
Billy-Vivant <billy.vivant@gmail.com>
2019-01-04 09:18:53 +0100

    Company edit done

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -8,6 +8,7 @@ use App\Entity\Job;
 use App\Repository\JobRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
