24c84e7366a97a814ccb08c9939783ccd02d30cd
Amelie Aumont <maka.amelie@hotmail.com>
2019-01-07 10:04:22 +0100

    chargement JS ok

--- assets/js/showMore.js
+++ assets/js/showMore.js
@@ -1,16 +1,17 @@
 
-
 $(function () {
 
     $(".one-comment").each( (id) => {
+        id++;
         let commentElt = $(".one-comment:nth-child("+id+")");
         if(id>3) commentElt.hide();
     });
     $("#showMore").on('click', function (e) {
         e.preventDefault();
-        $(".one-comment:hidden").slice(0, 4).slideDown();
+        $(".one-comment:hidden").slice(0, 3).slideDown();
         if ($(".one-comment:hidden")) {
             $("#load").fadeOut('slow');
         }
     });
+
 });
