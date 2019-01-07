ac448c0f9c86f8f4e28db01df9cbc722ad217f25
Billy-Vivant <billy.vivant@gmail.com>
2019-01-07 15:08:19 +0100

    rectification pour Travis

--- src/Controller/PartnerAdminController.php
+++ src/Controller/PartnerAdminController.php
@@ -13,7 +13,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 /**
  * @Route("/admin/partner")
  */
