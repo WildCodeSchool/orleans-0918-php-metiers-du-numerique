81d2d525c75c0b6341abdaf2c09cab297b5130e0
Amelie Aumont <maka.amelie@hotmail.com>
2019-01-08 14:47:21 +0100

    set default

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -61,9 +61,9 @@ class Company
 
 
     /**
-     * @ORM\Column(type="boolean", options={"default"=0}, nullable=true)
+     * @ORM\Column(type="boolean", options={"default"=false}, nullable=false)
      */
-    private $accepted;
+    private $accepted =false;
 
     public function __construct()
     {
