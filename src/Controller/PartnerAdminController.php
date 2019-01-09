76031b72e06f2bde98501a5820a75f15e47e4f20
Thomas Pecout <pecoutt@hotmail.fr>
2019-01-09 08:58:41 +0100

    pagination partner

--- src/Controller/PartnerAdminController.php
+++ src/Controller/PartnerAdminController.php
@@ -9,7 +9,9 @@
 namespace App\Controller;
 
 use App\Repository\PartnerRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,8 +23,17 @@ class PartnerAdminController extends AbstractController
     /**
      * @Route("/", name="partner_admin_index", methods="GET")
      */
-    public function index(PartnerRepository $partnerRepository): Response
-    {
-        return $this->render('partner_admin/index.html.twig', ['partners' => $partnerRepository->findAll()]);
+    public function index(
+        PartnerRepository $partnerRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $pagination = $paginator->paginate(
+            $partnerRepository->findAll(),
+            $request->query->getInt('page', 1),
+        $this->getParameter('elements_by_page')
+        );
+        return $this->render('partner_admin/index.html.twig', ['partners' => $pagination
+        ]);
     }
 }
