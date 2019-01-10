8a1ceb1c0d918c58ef6e88fc8794e55cf30af3bd
Thomas Pecout <pecoutt@hotmail.fr>
2019-01-10 17:15:10 +0100

    suppression et modification vue inutile

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -55,28 +55,6 @@ class JobController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="job_new", methods="GET|POST")
-     */
-    public function new(Request $request): Response
-    {
-        $job = new Job();
-        $form = $this->createForm(JobType::class, $job);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($job);
-            $em->flush();
-            return $this->redirectToRoute('job_index');
-        }
-
-        return $this->render('job/new.html.twig', [
-            'job' => $job,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
      * @Route("/{id}", name="job_show", methods="GET|POST")
      */
     public function show(Job $job, Request $request, SessionInterface $session): Response
@@ -93,38 +71,5 @@ class JobController extends AbstractController
             'searchForm' =>
                 SearchFormTrait::getForm($request, $this->get('form.factory'), $this->get('router'))->createView(),
         ]);
-    }
-    /**
-     * @Route("/{id}/edit", name="job_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, Job $job): Response
-    {
-        $form = $this->createForm(JobType::class, $job);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('job_index', ['id' => $job->getId()]);
-        }
-
-        return $this->render('job/edit.html.twig', [
-            'job' => $job,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="job_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Job $job): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$job->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($job);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('job_index');
     }
 }
