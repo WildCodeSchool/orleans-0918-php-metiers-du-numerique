92b4ab0e1c5422bbbc27e1e3c1280498ead5cbbf
Thomas Pecout <pecoutt@hotmail.fr>
2019-01-14 16:13:04 +0100

    done

--- src/Form/LegalNoticeType.php
+++ src/Form/LegalNoticeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\LegalNotice;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class LegalNoticeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Notice')
+            ->add('notice', TextareaType::class, [
+                'label' => 'Mentions légales',
+                'attr'=> ['class' => 'color-input']
+            ])
         ;
     }
 
